afb7110dab1a0013371f521402869c4df28efb3f
Phuong Danh - Global <tuphuongdanh1996@gmail.com>
2016-06-28 10:19:33 +0700

    Cau hinh laravel

---
+++ app/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table = 'products';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'slug' , 'price', 'saleprice', 'image', 'intro', 'quantity','liked', 'viewed', 'saled', 'description', 'cate_id',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    
+
+    public $timestamps = true;
+}
